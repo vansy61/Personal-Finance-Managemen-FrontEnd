414e79ebe4531eced10160c78b4616a41768e112
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-08-07 14:08:29 +0700

    add status sent reset password email

--- src/Apis/AuthApi.js
+++ src/Apis/AuthApi.js
@@ -5,6 +5,15 @@ class AuthApi {
     return axiosInstance.post('/api/v1/public/login', data);
   }
 
+
+  static forgotPassword(data) {
+    return axiosInstance.post('/api/v1/public/forgot-password', data, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+  }
+
 }
 
 export default AuthApi;
