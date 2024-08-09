342ba78085836161b30ae59d420c732af6087c17
PhamQuang123 <phamtienquang57@gmail.com>
2024-08-09 15:40:53 +0700

    update get all transaction

--- src/Pages/Transaction/TransactionItem.js
+++ src/Pages/Transaction/TransactionItem.js
@@ -1,8 +1,8 @@
-function TransactionItem({transactions, i}) {
+function TransactionItem({transaction, i}) {
     return (
         <div className="transaction-item bg-white shadow-sm p-3 rounded-3 d-flex align-items-center mb-2">
             <div className="border-end pe-3">
-                <span className="badge light badge-danger">Chi</span>
+                <span className="badge light badge-danger">{transaction.categoryType == 1? "Thu":"Chi"}</span>
             </div>
             <div className="border-end px-3">
                 <div className="d-flex align-items-center">
@@ -10,22 +10,22 @@ function TransactionItem({transactions, i}) {
                         <img src="/images/icons/icon_5.png" className="img-fluid" style={{width: "50px"}}/>
                     </div>
                     <div className="ms-3">
-                        <h5 className="mb-0">Tiền teesst</h5>
-                        <p className="mb-0"><small>03:30 12/10.2024</small></p>
+                        <h5 className="mb-0">{transaction.categoryName}</h5>
+                        <p className="mb-0"><small>{transaction.datetime}</small></p>
                     </div>
                 </div>
             </div>
             <div className="border-end px-3">
                 <i className="fa-solid fa-wallet"></i>
-                <span className="ms-2 fs-14">Ví Tiền Mặt</span>
+                <span className="ms-2 fs-14">{transaction.walletName}</span>
             </div>
 
             <div className="border-end px-3">
                 <i className="fa-solid fa-money-bill"></i>
-                <span className="ms-2 fs-14">200.000 đ</span>
+                <span className="ms-2 fs-14">{transaction.amount} đ</span>
             </div>
             <div className="px-3">
-                <span className="ms-2 fs-14">Đây là ghi chú nè ghi chú nè ghi chú nè ...</span>
+                <span className="ms-2 fs-14">{transaction.note}</span>
             </div>
             <div className="flex-grow-1 ps-3 text-end">
                 <button type="button" className="btn btn-rounded btn-outline-secondary btn-sm p-2">
